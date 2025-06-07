37a807ec89bd38e37c69837699ceb32bf61d8eb6
prajwal-dev <prajwaldilipbhagat@gmail.com>
2025-06-07 20:56:47 +0530

    new files added

---
+++ jsPractice.js
@@ -0,0 +1,61 @@
+// let fruits = ["apple", "orange", "mango", "grapes", "banana"];
+//* for of loop
+// for (let item of fruits){
+//     console.log(item)
+// }
+//* for in loop
+// for (let item2 in fruits){
+//     console.log(item2)
+// }
+
+// * ForEach
+
+// const myvar=fruits.forEach((curElem,index,arr)=>{
+//     // console.log(arr)
+//     console.log(`${curElem} ${index} ${arr}`)
+// })
+// console.log(arr)
+//* .map()
+// const myvar1=fruits.map((curElem,index,arr)=>{
+//     // console.log(arr)
+//   return ` my fav fruit is ${curElem}`;
+
+// })
+// console.log(myvar1)
+// console.log(fruits)
+
+//! write a program to Multiply each element with 2
+// const numbers=[1,2,3,4,5];
+// const multiply=numbers.map((curElem,index,arr)=>{
+//     console.log(curElem*2); 
+// })
+// console.log(multiply)
+
+
+// const multiply2=numbers.map((curElem,index,arr)=>{
+//     return curElem*2; 
+// })
+// console.log(multiply2)
+
+// const multiply=numbers.forEach((curElem,index,arr)=>{
+//     console.log(curElem*2); 
+// })
+// console.log(multiply)
+
+// let fruits = ["apple", "orange", "banana", "mango"];
+// fruits.splice(1, 1, "grapes","kiwi");
+// fruits.splice(4, 0, "grapes");
+// console.log(fruits);
+
+// const numbers = [1, 2, 3, 4, 6, 5, 6, 7,6, 8, 9];
+
+// console.log(numbers.includes(6));
+//todo Challenge time
+//? 1: Add Dec at the end of an array?
+//? 2: What is the return value of splice method?
+//? 3: Update march to March (update)?
+//? 4: Delete June from an array?
+
+const months = ["Jan", "march", "April", "June", "July"];
+
+const arr= months.splice()

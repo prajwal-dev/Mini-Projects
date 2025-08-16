5d688d2f2b0d42478940e398e9fd36f8f9b4833a
prajwal-dev <prajwaldilipbhagat@gmail.com>
2025-08-16 23:49:05 +0530

    add

--- js_practice/array.js
+++ js_practice/array.js
@@ -8,6 +8,31 @@ let arr = [1, 2, 3, 4];
 //   console.log(arr[i] * 2);
 // }
 
-for (let elem of arr) {
-  console.log(elem);
-}
+// for (let elem of arr) {
+//   console.log(elem * 3);
+// }
+
+//! spread operator
+
+let arr2 = ["prajwal", "bhagat", "qwerty"];
+let arr3 = [...arr, ...arr2];
+console.log(arr3);
+
+// ! join()
+
+// let fruits = ["mango", "litchi", "guava"];
+// console.log(fruits.join(" * "));
+// console.log(fruits.indexOf("guava"));
+// let bool = fruits.includes("litchi");
+// console.log(bool);
+// fruits.reverse();
+// console.log(fruits);
+
+// ! flat()
+let heroes = [
+  "spiderman",
+  "hulk",
+  ["krish", "shaktiman", ["thor"]],
+  "superman",
+];
+console.log(heroes.flat());

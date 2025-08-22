0fb5da909c523576495da829c1eb1104ec958180
prajwal-dev <prajwaldilipbhagat@gmail.com>
2025-08-22 20:11:19 +0530

    add

--- js_practice/object.js
+++ js_practice/object.js
@@ -87,16 +87,87 @@
 
 // * Write a function that returns an array of names of users who have the role “admin”.
 
-const users = [
-  { name: "Alice", role: "admin" },
-  { name: "Bob", role: "user" },
-  { name: "Charlie", role: "admin" },
-];
-let filteredUser = users.filter((users) => {
-  return users.role === "admin";
-});
+// const users = [
+//   { name: "Alice", role: "admin" },
+//   { name: "Bob", role: "user" },
+//   { name: "Charlie", role: "admin" },
+// ];
 
-let resultArray = filteredUser.map((user) => {
-  return user.name;
-});
-console.log(...resultArray);
+// function filterArray() {
+//   let filteredUser = users.filter((users) => {
+//     return users.role === "admin";
+//   });
+
+//   let resultArray = filteredUser.map((user) => {
+//     return user.name;
+//   });
+//   console.log(resultArray);
+// }
+// filterArray();
+
+// * Write a function searchProducts(products, keyword) that returns an array of products whose nameincludes the given keyword (case-insensitive).
+
+// const products = [
+//   { id: 1, name: "iPhone 14" },
+//   { id: 2, name: "Samsung Galaxy" },
+//   { id: 3, name: "Google Pixel" },
+// ];
+// function search(products, searchKeyword) {
+//   let filteredArray = products.filter((elem) => {
+//     return elem.name.toLowerCase().includes(searchKeyword.toLowerCase());
+//   });
+//   console.log(filteredArray);
+// }
+// search(products, "i");
+// ! Problems
+// * Easy
+// * 1} Create an object person with properties name, age, and city. Print all keys and values.
+// let me = {
+//   name: "Prajwal",
+//   age: 25,
+//   city: "Pune",
+// };
+// console.log(Object.keys(me));
+// console.log(Object.values(me));
+
+// * 2} Check if an object is empty or not.
+// function isEmpty(obj) {
+//   return Object.keys(obj).length === 0;
+// }
+// console.log(isEmpty({}));
+// * 3} Merge two objects:
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { c: 3, d: 4 };
+// Expected: { a:1, b:2, c:3, d:4 }
+// const merged = { ...obj1, ...obj2 };
+// console.log(merged);
+// *4} Write a function that takes an object and returns an array of its keys.
+
+// let obj = { a: 1, b: 2, c: 3 };
+
+// function key(hey) {
+//   let abc = Object.keys(hey);
+//   console.log(abc);
+// }
+// key(obj);
+
+// * 5} Count how many properties an object has.
+// let obj = { a: 1, b: 2, c: 3, d: 5 };
+
+// function prop(obj) {
+//   let count = Object.keys(obj).length;
+//   console.log(count);
+// }
+// prop(obj);
+// ! Medium level
+// * 6} Given an object, swap its keys and values.
+// { a: 1, b: 2, c: 3 } → { 1: "a", 2: "b", 3: "c" }
+let obj = { a: 1, b: 2, c: 3 };
+function swap(obj) {
+  const swapped = {};
+  for (let key in obj) {
+    console.log(key);
+  }
+  return swapped;
+}
+swap(obj);
